43a96c5dca15369ecd5361ea2d7e30afe4311cff
hasan5323 <ibrahimhasan002@gmail.com>
2023-09-28 14:25:52 +0700

    reuse routes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcryptjs');
+
 const app = express();
 const port = 3000
+const router = require('./routes/routes')
+
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
@@ -10,48 +12,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
 }));
-
-// Temporary user data (replace with your user database)
-const users = [
-  {
-    username: 'user1',
-    passwordHash: bcrypt.hashSync('password1', 10),
-  },
-  // Add more users as needed
-];
-
-const sessionValidator = (req, res, next) => {
-    // Check if there is a session and if the user is authenticated
-    if (req.session && req.session.user) {
-        // Session is valid, the user is authenticated
-        next(); // Continue to the next middleware or route handler
-    } else {
-        // Session is not valid or the user is not authenticated
-        res.redirect('/login'); // Redirect to the login page or handle it as needed
-    }
-};
-
-// Login page
-app.get('/login', (req, res) => {
-  res.render('home');
-});
-
-// Login endpoint
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Find the user by username
-  const user = users.find(u => u.username === username); //findOne sequelize output objek
-  //output user line 43 isinya object sama dengan line 15-18
-  if (!user || !bcrypt.compareSync(password, user.passwordHash)) {
-    res.send('Invalid username or password.');
-  } else {
-    // Set the user session
-    req.session.user = user;
-    res.redirect('/welcome');
-  }
-});
-
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,46 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
 const Controller = require("../controllers/controller");
 const router = express.Router();
 
+// Login page
+router.get("/login", (req, res) => {
+  res.render("home");
+});
 
+// Login endpoint
+router.post("/login", (req, res) => {
+    // Temporary user data (replace with your user database)
+    const users = [
+        {
+          username: 'user1',
+          passwordHash: bcrypt.hashSync('password1', 10),
+        },
+        // Add more users as needed
+      ];
+      
+      const sessionValidator = (req, res, next) => {
+          // Check if there is a session and if the user is authenticated
+          if (req.session && req.session.user) {
+              // Session is valid, the user is authenticated
+              next(); // Continue to the next middleware or route handler
+          } else {
+              // Session is not valid or the user is not authenticated
+              res.redirect('/login'); // Redirect to the login page or handle it as needed
+          }
+      };
+  const { username, password } = req.body;
 
+  // Find the user by username
+  const user = users.find((u) => u.username === username); //findOne sequelize output objek
+  //output user line 43 isinya object sama dengan line 15-18
+  if (!user || !bcrypt.compareSync(password, user.passwordHash)) {
+    res.send("Invalid username or password.");
+  } else {
+    // Set the user session
+    req.session.user = user;
+    res.redirect("/welcome");
+  }
+});
 
-
-module.exports = router
+module.exports = router;
